7273d72ea22440243e9ddb2237dfbaa2c2d0eaac
Yazhong Liu <yorkiefixer@gmail.com>
2014-06-25 16:28:42 +0800

    complete select/fetch/disconnect commands flow and add an example

---
+++ example.js
@@ -0,0 +1,23 @@
+
+var IMAPSession = require('./lib/imap').IMAPSession;
+var imap = new IMAPSession(993, 'imap.gmail.com', {
+	secure: 'tls',
+	auth: {
+		username: 'yorkiefixer@gmail.com',
+		password: 'xxxxxxxxx'
+	}
+});
+
+imap.connect();
+imap.on('error', function(err) {
+	throw err;
+});
+imap.on('connect', function() {
+	console.log('connected');
+	imap.select('INBOX', function(inbox) {
+		console.log(inbox);
+	});
+	imap.fetchMessagesByUID('*', '*', null, function(messages) {
+		console.log(messages);
+	});
+});
